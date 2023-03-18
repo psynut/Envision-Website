4f9d288ed5888944347c4c1e30ee351379701f2d
Michael Sibrava <48391846+psynut@users.noreply.github.com>
2023-03-18 13:50:23 -0700

    Added the umbrella foreground and shooting stars
    index.js adjusts for resizing so the woman in foreground matches the background image.
    Shooting star pictures added to animate the shooting star.

--- index.js
+++ index.js
@@ -5,10 +5,24 @@ var animationReady = true;
 var laurel = document.getElementsByClassName("laureltilt")[0];
 var heart = document.getElementsByClassName("hearttilt")[0];
 var navbarIcon = document.getElementsByClassName("navbar-logo")[0];
+var title = document.getElementsByClassName("d-lg-inline-block envisionname")[0];
 
-var title = document.getElementsByClassName("d-lg-inline-block envisionname")[0]
+var shootingStarPic = ["images/ShootingStar0.png", "images/ShootingStar1.png", "images/ShootingStar2.png"];
+var cometPanel = document.getElementsByClassName("comet-panel")[0];
+var cometImage = document.createElement("img");
+cometPanel.appendChild(cometImage);
+cometImage.style.position = "absolute";
+cometImage.style.zIndex = 0;
+
+var umbrellaForeground = document.getElementsByClassName("umbrella-foreground")[0];
+
 title.addEventListener("mouseenter", forwardAnimation, {passive: true});
-//title.addEventListener("mouseleave", backwardAnimation, {passive: true});
+window.addEventListener("resize", matchDivSize);
+
+
+startCometAnimation();
+
+
 
 moveNavbarIcon(Date.now()/1000);
 
@@ -99,9 +113,53 @@ function moveNavbarIcon(startTime){
 
 }
 
-function backwardAnimation(){
-    laurel.style.left = "20px";
-    laurel.style.top = "20px";
+function startCometAnimation(){
+    let startLeft = Math.floor(Math.random()*1401);
+    let startTop = Math.floor(Math.random()*261)-10;
+    let direction = (Math.random() > 0.5);
+    cometImage.style.width = "100px";
+    cometImage.style.height = "50px";
+    cometImage.style.transform = direction ? "scaleX(1)" : "scaleX(-1)";
+    animateComet(startLeft, startTop, direction, Date.now()/1000)
+    swapCometImage(0);
+}
+
+function animateComet(startLeft, startTop, direction, startTime){
+    let animationDuration = 1.5;
+    let horizontalSpeed = 350;
+    let verticalSpeed = 100;
+    let elapsedTime = Date.now()/1000 - startTime;
+    horizontalSpeed = direction ? horizontalSpeed : -horizontalSpeed;
+    if(elapsedTime < animationDuration){
+        cometImage.style.left = numToPx(startLeft-(elapsedTime)*horizontalSpeed);
+        cometImage.style.top = numToPx(startTop+(.5+elapsedTime**2)*verticalSpeed);
+        setTimeout(()=>{animateComet(startLeft, startTop, direction, startTime)},16);
+    }else{
+        //cometImage.width = "40px";
+        //cometImage.height = "20px";
+    }
+}
+
+function swapCometImage(index){
+    if(index < 3){
+        cometImage.src = shootingStarPic[index];
+        setTimeout(()=>{swapCometImage(index+1)},400)
+    }else{
+        let delay = 5000+(Math.random()*5000);
+        cometImage.style.width = "0";
+        cometImage.style.height = "0";
+        setTimeout(startCometAnimation, 2000+Math.random()*5000);
+    }
+}
+
+//trying to figure out how to correctly adjust umbrellaForeground's top or bottom position after finding adjusting width.
+//Then add a listener for each time the screen dimension changes.
+//matchDivSize();
+setTimeout(matchDivSize, 20);
+function matchDivSize(){
+    console.log(cometPanel.getBoundingClientRect().width);
+    umbrellaForeground.style.width = numToPx(cometPanel.getBoundingClientRect().width*1.5);
+    umbrellaForeground.style.top = numToPx((cometPanel.getBoundingClientRect().height-umbrellaForeground.getBoundingClientRect().height)/2);
 }
 
 function numToPx(m_Int){
